81f94381b00dfecf3ba8f166a829d92359664c1d
Halex Viotto Gomes <halexx002@gmail.com>
2021-04-22 15:40:26 -0400

    Code from class 4

---
+++ src/http.ts
@@ -0,0 +1,42 @@
+// O import e export abaixo só funciona se adicionar no package.json o item type: module
+// Nesse projeto foi instalado o ts-node-dev -D para fazer isso funcionar. Foi criado um script dev: ts-node-dev src/server.js
+import express from "express";
+
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
+
+import "./database";
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (req, res) => {
+  return res.render("html/client.html");
+});
+
+const http = createServer(app); // Criando o protocolo http
+const io = new Server(http); // Criando o protocolo WS
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
+
+/* 
+  - GET = buscas
+  - POST = Criação
+  - PUT = Alteração
+  - DELETE = Deletar
+  - PATCH = Alterar uma informação específica
+*/
+
+app.use(express.json());
+
+app.use(routes);
+
+export { http, io };
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,4 @@
-// O import e export abaixo só funciona se adicionar no package.json o item type: module
-// Nesse projeto foi instalado o ts-node-dev -D para fazer isso funcionar. Foi criado um script dev: ts-node-dev src/server.js
-import express from "express"
+import { http } from "./http";
+import "./websocket/client";
 
-import "./database"
-import { routes } from "./routes" 
-
-const app = express()
-
-/* 
-  - GET = buscas
-  - POST = Criação
-  - PUT = Alteração
-  - DELETE = Deletar
-  - PATCH = Alterar uma informação específica
-*/
-
-app.use(express.json())
-
-app.use(routes)
-
-app.listen(3333, () => console.log(`Server is running on port: 3333`))
+http.listen(3333, () => console.log(`Server is running on port: 3333`));
